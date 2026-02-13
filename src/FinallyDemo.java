a6a52123efa46b9642fa1a07b9df346d533fab9c
Gauricode <gaurijkjay@gmail.com>
2026-02-13 10:07:27 +0530

    error

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class FinallyDemo {
-    
+    public static void main(String [] args){
         // TODO: Read a and b
-        
+        Scanner sc = new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	try{
+		System.out.println("Result: " + a/b);
+	}
+	catch(ArithmeticException e){
+		System.out.println("Error: " + e.getMessage());
+
+	}
+	finally{
+		System.out.println("Finally block executed");
+	}
         // TODO: try block
         // Perform division
         // Print "Result: " + result
